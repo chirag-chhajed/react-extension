8e569ebe4449347ebf75f2708a7e8a46d2d6e15d
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-10-15 00:51:59 +0530

    temp site added

--- app/site/tailwind.config.js
+++ app/site/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {},
   },
   plugins: [],
