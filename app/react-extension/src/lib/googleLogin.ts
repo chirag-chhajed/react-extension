41ef851e95d7a109537f3a906861131d3fdbfade
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-10-17 12:13:49 +0530

    added google auth support for other browsers

--- app/react-extension/src/lib/googleLogin.ts
+++ app/react-extension/src/lib/googleLogin.ts
@@ -4,10 +4,41 @@ const getGoogleAuthCredential = () => {
   return new Promise<ReturnType<typeof GoogleAuthProvider.credential>>(
     (resolve, reject) => {
       chrome.identity.getAuthToken({ interactive: true }, (token) => {
-        // console.log(token, "token");
         if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError);
-          reject(chrome.runtime.lastError);
+          try {
+            const redirectURL = chrome.identity.getRedirectURL();
+            const clientID =
+              "240104267874-0b34rikbkjoquq7pj5m4h5uq0csrah4m.apps.googleusercontent.com";
+            const scopes = ["email", "profile"];
+            let authURL = "https://accounts.google.com/o/oauth2/auth";
+            authURL += `?client_id=${clientID}`;
+            authURL += `&response_type=token`;
+            authURL += `&redirect_uri=${encodeURIComponent(redirectURL)}`;
+            authURL += `&scope=${encodeURIComponent(scopes.join(" "))}`;
+            chrome.identity.launchWebAuthFlow(
+              {
+                interactive: true,
+                url: authURL,
+              },
+              (redirect_uri) => {
+                if (chrome.runtime.lastError) {
+                  console.error(chrome.runtime.lastError.message);
+                  reject(chrome.runtime.lastError.message);
+                  return;
+                }
+                const url = new URL(redirect_uri!);
+                const id_token = url.hash.split("&")[0].split("=")[1];
+                const credential = GoogleAuthProvider.credential(
+                  null,
+                  id_token
+                );
+                resolve(credential);
+              }
+            );
+          } catch (error) {
+            console.error(error);
+            reject(error);
+          }
         }
         const credential = GoogleAuthProvider.credential(null, token);
         resolve(credential);
